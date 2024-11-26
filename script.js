fea9be99a52b78c692025816e267a847c1f3bc32
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-26 12:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeselectorcolor() {
+	const colorSelect = document.getElementById("colorSelect");
+
+	 const selectedIndex = colorSelect.selectedIndex;
+
+	 if (selectedIndex !== -1) {
+    colorSelect.options[selectedIndex].remove();
+  }
+	
+}
